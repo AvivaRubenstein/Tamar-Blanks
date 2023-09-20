25960bfa7a207012e0f338f99baac7420844428a
Aviva <avivadrubenstein@gmail.com>
2023-09-20 19:53:14 -0400

    styling navbar, including style changes for mobile vs desktop responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+require('../Assets/styles/header.css');
 
 export default function Header(){
     return (
         <header className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="navbar-brand">Tamar Blanks</div>
-            <Link className="navLink" to="/"> Home </Link>
-            <Link className="navLink" to='/Research'> Research </Link>
-            <Link className="navLink" to='/Teaching'> Teaching </Link>
-            <Link className="navLink" to='/Outreach'> Outreach </Link>
-            <button>CV</button>
+            <Link className="navLink navbar-item" to="/"> Home </Link>
+            <Link className="navLink navbar-item" to='/Research'> Research </Link>
+            <Link className="navLink navbar-item" to='/Teaching'> Teaching </Link>
+            <Link className="navLink navbar-item" to='/Outreach'> Outreach </Link>
+            <button id="cv-button" className="navbar-item">CV</button>
             {/* button for CV download here */}
         </header>
     )
