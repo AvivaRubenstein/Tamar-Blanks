939b572fa8a7d9f5563f8c95ffedec636b4122ef
Aviva <avivadrubenstein@gmail.com>
2023-09-20 17:48:36 -0400

    adding links to Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,13 +8,23 @@ const styles = {
 export default function Home() {
     return (
         <div>
-        <p>I am a mathematics PhD candidate and NSF Graduate Research Fellow at Rutgers University. My research is in algebra: I am interested in quadratic forms, fields, division algebras, and Lie algebras. My advisor is Danny Krashen.
-        <p>Here's a video of me talking about <a href="https://youtu.be/4AU8raNO7jE">cool generalizations of Schur's lemma.</a></p>
+        <p>I am a mathematics PhD candidate and NSF Graduate Research Fellow at Rutgers University. My research is in algebra: I am interested in quadratic forms, fields, division algebras, and Lie algebras. 
+            My advisor is Danny Krashen. 
+        </p>
+
+        <p>Here's a video of me talking about 
+            <a href="https://youtu.be/4AU8raNO7jE">cool generalizations of Schur's lemma.</a>
+        </p>
         
-
-I was a 2021 AMS-AAAS Mass Media Fellow at The Conversation US. For more on that, see Outreach.
-
-My email address is tl548 at math.rutgers.edu.</p>
+<p>
+I was a 2021 I was a 2021  
+<a href="https://www.aaas.org/programs/mass-media-fellowship/tamar-lichter-blanks"> AMS-AAAS Mass Media Fellow </a>
+at <a href="https://theconversation.com/us"> The Conversation US</a>
+. For more on that, see Outreach.
+</p>
+<p>
+My email address is tl548 at math.rutgers.edu.
+</p>
         <img src={photo} alt="photo" style={styles.photo}></img>
         </div>
     )
