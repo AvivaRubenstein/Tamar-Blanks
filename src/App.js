5bc3a98ea3b3f98adc4cfaa9c50e20ae3b496a81
Aviva <avivadrubenstein@gmail.com>
2023-09-21 15:28:09 -0400

    fixed filepath of App.js imports

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import './Assets/styles/root.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
+import Home from '.components/pages/Home';
 import Research from '.components/pages/Research';
 import Teaching from '.components/pages/Teaching';
 import Outreach from '.components/pages/Outreach';
