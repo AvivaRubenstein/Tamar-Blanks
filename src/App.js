5785fe8698752c6104b6c606981bf8e379d9f509
Aviva <avivadrubenstein@gmail.com>
2023-09-21 15:25:02 -0400

    testing update to fix routing issue

--- src/App.js
+++ src/App.js
@@ -2,13 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Assets/styles/root.css';
-import Container from './components/Container';
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Research from './pages/Research';
+import Teaching from './pages/Teaching';
+import Outreach from './pages/Outreach';
+import Header from './Header';
 function App() {
 
   return (
     <div className="App">
-     <Container/> 
+      (
+        <Router basename="/tamar-blanks">
+            <Header/>
+            <Routes>
+                <Route
+                exact path="/"
+                element={<Home />}
+                />
+                <Route
+                path="/Research"
+                element={<Research />}
+                />
+                 <Route
+                path="/Teaching"
+                element={<Teaching />}
+                />
+                 <Route
+                path="/Outreach"
+                element={<Outreach />}
+                />
+            </Routes>
+        </Router>
+    )
     </div>
   );
 }
