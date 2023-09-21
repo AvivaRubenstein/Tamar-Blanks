4b2e8addb90dbcafe432cea02db4eeb22269f812
Aviva <avivadrubenstein@gmail.com>
2023-09-21 14:47:13 -0400

    title update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Assets/styles/root.css';
 import Container from './components/Container';
 
 function App() {
-  // Use the useEffect hook to set the document title
-  useEffect(() => {
-    document.title = "tamar-blanks"; 
-  }, []);
 
   return (
     <div className="App">
