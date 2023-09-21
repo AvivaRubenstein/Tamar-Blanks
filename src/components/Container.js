6bf8526abdce2457c2ba44895d15853a03541381
Aviva <avivadrubenstein@gmail.com>
2023-09-21 12:09:19 -0400

    edited basename for gh-pages

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,7 +8,7 @@ import Header from './Header';
 
 export default function Container() {
     return (
-        <Router >
+        <Router basename="/tamar-blanks">
             <Header/>
             <Routes>
                 <Route
