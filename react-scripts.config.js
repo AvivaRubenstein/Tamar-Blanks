9fdc9a8d7c6daccd41cb59ab98fe5147ffa9c70a
Aviva <avivadrubenstein@gmail.com>
2023-09-21 15:09:05 -0400

    removed conditional logic which would only work in production environment

--- react-scripts.config.js
+++ react-scripts.config.js
@@ -1,8 +1,6 @@
 // react-scripts.config.js
 export default function override(config, env) {
     // Customize your build configuration here
-    if (env === 'production') {
       config.output.filename = 'static/js/main.js'; // Set the desired filename
-    }
     return config;
   };
