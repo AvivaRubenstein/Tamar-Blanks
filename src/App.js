813ba788bf2e601c93f1ae50c9f1b34b5887b354
Aviva <avivadrubenstein@gmail.com>
2023-09-21 14:27:11 -0400

    updating site's title in html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Assets/styles/root.css';
 import Container from './components/Container';
 
 function App() {
+  // Use the useEffect hook to set the document title
+  useEffect(() => {
+    document.title = "tamar-blanks"; // Replace with your desired title
+  }, []);
+
   return (
     <div className="App">
      <Container/> 
