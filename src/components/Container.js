046f6fd9a3aaea0275d0849311a473e38053c666
Aviva <avivadrubenstein@gmail.com>
2023-09-21 12:06:04 -0400

    removing basename in Router

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,7 +8,7 @@ import Header from './Header';
 
 export default function Container() {
     return (
-        <Router basename="/">
+        <Router >
             <Header/>
             <Routes>
                 <Route
